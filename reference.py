6e23685580bd8e7695ae9b152f8b6ef80c342c9a
Louis Liao <buzonliao@gmail.com>
2019-05-23 10:09:47 +0800

    add string reference

---
+++ reference.py
@@ -0,0 +1,32 @@
+import random
+
+# tuple
+# n = ("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")[3]
+# print(n)
+# toast = "PYTHONSLICE"
+# tuples=toast[0:3],toast[3:6],toast[6:9],toast[9:11]
+# print(tuples)
+
+# list
+# n = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][random.randint(0,6)]
+# print(n)
+# n = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+# n[2] = "星期二"
+# print(n)
+
+# dic
+# days={
+#   1:"Sunday",
+#   2:"Monday",
+#   3:"Tuesday",
+#   4:"Wednesday",
+#   5:"Thursday",
+#   6:"Friday",
+#   7:"Saturday"
+#   }
+# n = days[2],days[3],days[4]
+# print(n)
+ 
+# %* passes all arguments
+# money=987.98
+# print("$%*.2f" % (7, money))
