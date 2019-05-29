677e11f3c14404b2744ad8c750d37289a9f6129d
louis.liao <buzonliao@gmail.com>
2019-05-30 00:24:18 +0800

    update hw1

--- lecture1/hw1.py
+++ lecture1/hw1.py
@@ -12,16 +12,16 @@ print(passEnglish - passMath)
 print(passMath & passEnglish)
 
 # all students
-print(passMath | passEnglish)
+print(len(passMath | passEnglish))
 
 # Q2
-grades = {
+students = {
     "Tom": [80, 100, 90, 95],
     "John": [100, 93, 75, 80]
 }
 
-avgTom = float(sum(grades["Tom"])) / len(grades["Tom"])
-avgJohn = float(sum(grades["John"])) / len(grades["John"])
+avgTom = sum(students["Tom"]) / len(students["Tom"])
+avgJohn = sum(students["John"]) / len(students["John"])
 
 print("Tom\'s average grade is: {}".format(avgTom))
 print("John\'s average grade is: {}".format(avgJohn))
