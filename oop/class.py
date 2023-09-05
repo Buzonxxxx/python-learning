a8f94e7dc82b66aefc06dc31481118694396ecb1
Louis Liao <buzonliao@gmail.com>
2023-09-05 11:08:57 +0800

    add oop sample code

---
+++ oop/class.py
@@ -0,0 +1,37 @@
+class PlayerCharacter:
+    # Class Object Attribute: use when the value won't change in different instances
+    membership = True
+    
+    # Constructor
+    def __init__(self, name, age):
+        if PlayerCharacter.membership:
+            self.name = name # attributes
+            self.age = age
+
+    def shout(self):
+        return f'my name is {self.name}'
+
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return cls('Teddy', num1 + num2)
+    
+    @staticmethod
+    def adding_things2(num1, num2):
+        return num1 + num2
+
+# class method / static method 可以不用instantiate instance
+print(PlayerCharacter.adding_things2(2,3)) #5
+
+player1 = PlayerCharacter('Louis', 39)
+player1.attack = 50
+
+print(player1.name) # Louis
+print(player1.attack) # 50
+print(player1.shout()) # my name is Louis
+print(player1.adding_things2(2,3)) # 5
+
+player3 = PlayerCharacter.adding_things(2,3) # 5
+print(player3.age)
+
+# help(player1)
+# help(str)
---
+++ oop/encapsulate.py
@@ -0,0 +1,4 @@
+# [Encapsulate]
+# Bundle the data and the methods that operate on that data into a single unit called 
+# a clas
+
