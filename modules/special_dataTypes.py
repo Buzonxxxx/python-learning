2026efdb6b437f9620a920f80e79f40186a0c093
Louis Liao <buzonliao@gmail.com>
2023-10-13 16:06:59 +0800

    twitter bot

--- modules/special_dataTypes.py
+++ modules/special_dataTypes.py
@@ -1,10 +1,10 @@
 from collections import Counter, defaultdict
 
 # Counter
-li = [1,2,3,4,5,6,7,7]
+li = [1,2,3,4,5,6,7,7] 
 sentence = 'bla bla bla'
-print(Counter(li))
-print(Counter(sentence))
+print(Counter(li)) # Counter({7: 2, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1})
+print(Counter(sentence)) # Counter({'b': 3, 'l': 3, 'a': 3, ' ': 2})
 
 # defaultdict
 dictionary = defaultdict(lambda: 'does not exist', {'a': 1, 'b': 2})
