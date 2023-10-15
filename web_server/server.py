0e334ba745f8ecee86297ca38afe4296f7a1afd4
Louis Liao <buzonliao@gmail.com>
2023-10-15 20:58:03 +0800

    update

--- web_server/server.py
+++ web_server/server.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, oops!</p>"
+@app.route('/')
+@app.route('/index.html')
+def home():
+    return render_template('./index.html')
+
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def losubmit_formgin():
+    return 'for submitted hoooooray!'
